ecd6f974dc62e4f58c23e622749c0b1498d4f7b2
domagoj <domagoj.bulat.zg@gmail.com>
2015-12-29 02:26:42 +0100

    removed unnecessary comments and indented TranspositionTable.c

--- TranspositionTable.c
+++ TranspositionTable.c
@@ -13,9 +13,7 @@
 #define LM UINT64_C(0x7FFFFFFF) /* Least significant 31 bits */
 
 U64 zobrist[782];
-
 nTTentry *nTT = NULL;
-
 
 /* The array for the state vector */
 static U64 mt[NN]; 
@@ -25,48 +23,46 @@ static int mti=NN+1;
 /* initializes mt[NN] with a seed */
 void init_genrand64(U64 seed)
 {
-    mt[0] = seed;
-    for (mti=1; mti<NN; mti++) 
-        mt[mti] =  (UINT64_C (6364136223846793005) * (mt[mti-1] ^ (mt[mti-1] >> 62)) + mti);
+	mt[0] = seed;
+	for (mti=1; mti<NN; mti++) 
+		mt[mti] =  (UINT64_C (6364136223846793005) * (mt[mti-1] ^ (mt[mti-1] >> 62)) + mti);
 }
-
 
 /* generates a random number on [0, 2^64-1]-interval */
 U64 genrand64_int64(void)
 {
-    int i;
-    U64 x;
-    static U64 mag01[2]={ 0, 0};
+	int i;
+	U64 x;
+	static U64 mag01[2]={ 0, 0};
 
-    if (mti >= NN) { /* generate NN words at one time */
+	if (mti >= NN) { /* generate NN words at one time */
+		/* if init_genrand64() has not been called, */
+		/* a default initial seed is used     */
+		if (mti == NN+1) 
+			init_genrand64(UINT64_C(5489)); 
 
-        /* if init_genrand64() has not been called, */
-        /* a default initial seed is used     */
-        if (mti == NN+1) 
-            init_genrand64(UINT64_C(5489)); 
+		for (i=0;i<NN-MM;i++) {
+			x = (mt[i]&UM)|(mt[i+1]&LM);
+			mt[i] = mt[i+MM] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
+		}
+		for (;i<NN-1;i++) {
+			x = (mt[i]&UM)|(mt[i+1]&LM);
+			mt[i] = mt[i+(MM-NN)] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
+		}
+		x = (mt[NN-1]&UM)|(mt[0]&LM);
+		mt[NN-1] = mt[MM-1] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
 
-        for (i=0;i<NN-MM;i++) {
-            x = (mt[i]&UM)|(mt[i+1]&LM);
-            mt[i] = mt[i+MM] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
-        }
-        for (;i<NN-1;i++) {
-            x = (mt[i]&UM)|(mt[i+1]&LM);
-            mt[i] = mt[i+(MM-NN)] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
-        }
-        x = (mt[NN-1]&UM)|(mt[0]&LM);
-        mt[NN-1] = mt[MM-1] ^ (x>>1) ^ mag01[(int)(x&UINT64_C(1))];
+		mti = 0;
+	}
 
-        mti = 0;
-    }
-  
-    x = mt[mti++];
+	x = mt[mti++];
 
-    x ^= (x >> 29) & UINT64_C(0x5555555555555555);
-    x ^= (x << 17) & UINT64_C(0x71D67FFFEDA60000);
-    x ^= (x << 37) & UINT64_C(0xFFF7EEE000000000);
-    x ^= (x >> 43);
+	x ^= (x >> 29) & UINT64_C(0x5555555555555555);
+	x ^= (x << 17) & UINT64_C(0x71D67FFFEDA60000);
+	x ^= (x << 37) & UINT64_C(0xFFF7EEE000000000);
+	x ^= (x >> 43);
 
-    return x;
+	return x;
 }
 
 
@@ -78,136 +74,102 @@ void initZobrist()
 	for (it = 0; it < 782; it++)
 	{
 		zobrist[it] = genrand64_int64();
-
 	}
 }
 
 void nsetZobrist( Nboard *b)
 {
-        int it, enp;
-        U64 m = 1ULL;
+	int it, enp;
+	U64 m = 1ULL;
 
-        b->zobrist = 0ULL;
+	b->zobrist = 0ULL;
 
-        for ( it = 0; it < 64; it++)
-        {
-                if (b->pieceset[8] & (m << it) )  b->zobrist ^= zobrist[ it];
-                else if (b->pieceset[9] & (m << it)) b->zobrist ^= zobrist[ 64 + it];
-                else if (b->pieceset[10] & (m << it)) b->zobrist ^= zobrist[ 2*64 + it];
-                else if (b->pieceset[11] & (m << it)) b->zobrist ^= zobrist[ 3*64 + it];
-                else if (b->pieceset[12] & (m << it)) b->zobrist ^= zobrist[ 4*64 + it];
-                else if (b->pieceset[13] & (m << it)) b->zobrist ^= zobrist[ 5*64 + it];
-                else if (b->pieceset[0] & (m << it)) b->zobrist ^= zobrist[ 6*64 + it];
-                else if (b->pieceset[1] & (m << it)) b->zobrist ^= zobrist[ 7*64 + it];
-                else if (b->pieceset[2] & (m << it)) b->zobrist ^= zobrist[ 8*64 + it];
-                else if (b->pieceset[3] & (m << it)) b->zobrist ^= zobrist[ 9*64 + it];
-                else if (b->pieceset[4] & (m << it)) b->zobrist ^= zobrist[ 10*64 + it];
-                else if (b->pieceset[5] & (m << it)) b->zobrist ^= zobrist[ 11*64 + it];
-        }
-        b->zobrist ^= (b->info >> 4 & 1ULL) ? zobrist[ 778] : 0ULL;
-        b->zobrist ^= (b->info >> 5 & 1ULL) ? zobrist[ 779] : 0ULL;
-        b->zobrist ^= (b->info >> 6 & 1ULL) ? zobrist[ 780] : 0ULL;
-        b->zobrist ^= (b->info >> 7 & 1ULL) ? zobrist[ 781] : 0ULL;
+	for ( it = 0; it < 64; it++)
+	{
+		if (b->pieceset[8] & (m << it) )  b->zobrist ^= zobrist[ it];
+		else if (b->pieceset[9] & (m << it)) b->zobrist ^= zobrist[ 64 + it];
+		else if (b->pieceset[10] & (m << it)) b->zobrist ^= zobrist[ 2*64 + it];
+		else if (b->pieceset[11] & (m << it)) b->zobrist ^= zobrist[ 3*64 + it];
+		else if (b->pieceset[12] & (m << it)) b->zobrist ^= zobrist[ 4*64 + it];
+		else if (b->pieceset[13] & (m << it)) b->zobrist ^= zobrist[ 5*64 + it];
+		else if (b->pieceset[0] & (m << it)) b->zobrist ^= zobrist[ 6*64 + it];
+		else if (b->pieceset[1] & (m << it)) b->zobrist ^= zobrist[ 7*64 + it];
+		else if (b->pieceset[2] & (m << it)) b->zobrist ^= zobrist[ 8*64 + it];
+		else if (b->pieceset[3] & (m << it)) b->zobrist ^= zobrist[ 9*64 + it];
+		else if (b->pieceset[4] & (m << it)) b->zobrist ^= zobrist[ 10*64 + it];
+		else if (b->pieceset[5] & (m << it)) b->zobrist ^= zobrist[ 11*64 + it];
+	}
+	b->zobrist ^= (b->info >> 4 & 1ULL) ? zobrist[ 778] : 0ULL;
+	b->zobrist ^= (b->info >> 5 & 1ULL) ? zobrist[ 779] : 0ULL;
+	b->zobrist ^= (b->info >> 6 & 1ULL) ? zobrist[ 780] : 0ULL;
+	b->zobrist ^= (b->info >> 7 & 1ULL) ? zobrist[ 781] : 0ULL;
 
-        enp = b->info >> 1 & 0x0000000000000007;
-        b->zobrist ^= b->info & 1ULL ? zobrist[ 768 + enp] : 0ULL;
+	enp = b->info >> 1 & 0x0000000000000007;
+	b->zobrist ^= b->info & 1ULL ? zobrist[ 768 + enp] : 0ULL;
 
-        b->zobrist ^= (b->info >> 14) & 1ULL ? zobrist[ 777] : 0ULL;
-
+	b->zobrist ^= (b->info >> 14) & 1ULL ? zobrist[ 777] : 0ULL;
 }
 
 U64 setnTT( U64 n)
 {
-        U64 count;
-        n *= 1024*1024;
+	U64 count;
+	n *= 1024*1024;
 
-        count = LargestPrime( n / sizeof(nTTentry) );
+	count = LargestPrime( n / sizeof(nTTentry) );
+	nTT = calloc( sizeof( nTTentry), count);
 
-        nTT = calloc( sizeof( nTTentry), count);
-
-        return count;
+	return count;
 }
 
 nTTentry *nTTlookup(U64 key)
 {
-        unsigned ind = key % count_nTT;
-	//printf("*\n");
-        if (nTT[ ind].zobrist != key) return NULL;
-	//printf("h\n");
-        TThit++;
-        return &nTT[ ind];
+	unsigned ind = key % count_nTT;
+	if (nTT[ ind].zobrist != key) 
+		return NULL;
+	TThit++;
+	return &nTT[ ind];
 }
 
 void nTTstore( U64 zobrist, U64 data)
 {
 	//always replace strategy
 	U64 ind = zobrist % count_nTT;
-	
-	/*if (nTT[ind].zobrist)
-	{
-		TTowr++;
-		if (((nTT[ ind].data >> 40) & 0x0000000000000003) == 1)
-			return;
-	}*/
 
 	TTwr++;
 	nTT[ ind].zobrist = zobrist;
 	nTT[ ind].data = data;
-
-/*                unsigned d, flag;
-                int score;
-		Nmove pick;
-
-                d = data & 0x000000000000FF;
-                flag = (data >> 24) & 0x00000000003;
-                score = (data >> 8) & 0x00000000FFFF;
-		pick = (data >> 26) & 0x00000001FFFFF;*/
-		
-		//if (flag != 1) return;
-	
-		/*printf("-----------\n");	
-                printmoveN( &pick);
-
-                printmovedetailsN( &pick);
-                printBits( sizeof(U64), &data);
-                printf("*nTTstore* flag %d score %d depth %d\n", flag, score, d);
-                printf("zobrist: %llu\n", zobrist);
-
-	printf("---------------------------\n");*/
 }
 
 void printNline( Nline pline)
 {
-    int it;
-    for (it = 0; pline.cmove > it; it++)
-    {
+	int it;
+	for (it = 0; pline.cmove > it; it++)
+	{
 		//if (pline.argmove[it].info & 1ULL)	printf(" #%d ", it);
 		printmoveN( &pline.argmove[it]);
-    }
-    printf("\n");
+	}
+	printf("\n");
 }
 
 void print_TTentry( nTTentry *arg, Nboard pos)
 {
-	
 }
 
 Nmove nTTextractPV( Nboard pos, char n)
 {
-        char i;
-        nTTentry *entry;
-        Nmove PV = NULL, pick = NULL;
+	char i;
+	nTTentry *entry;
+	Nmove PV = NULL, pick = NULL;
 
 	for ( i = 0; i < n; i++)
-        {
-                entry = nTTlookup( pos.zobrist);
-                if (!entry)   
+	{
+		entry = nTTlookup( pos.zobrist);
+		if (!entry)   
 		{
 			printf("!!pvN: %d	", i);
 			printmoveN(&pick);
 			printf("\n");
 			return pick;
-
 		}
 
 		TThit--;
@@ -224,18 +186,16 @@ Nmove nTTextractPV( Nboard pos, char n)
 		}
 
 		printmoveN( &PV);
-		
-                Ndo_move( &pos, PV);
 
-	        }
-        printf("\n");
-
-        return pick;
+		Ndo_move( &pos, PV);
+	}
+	printf("\n");
+	return pick;
 }
 
 Nmove TTfind_move( U64 key)
 {
-        nTTentry *entry;
+	nTTentry *entry;
 
 	entry = nTTlookup( key);
 	if (!entry)   
@@ -243,9 +203,7 @@ Nmove TTfind_move( U64 key)
 		printf("!!pv_find error\n");
 		return 0;
 	}
-
 	TThit--;
-
 	return (entry->data >> 0) & 0x00000003FFFFFF;
 }
 
